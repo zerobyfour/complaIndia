7137d461e5f7a53bd206ffa3cd942137cd42675d
Rahul Kumar <27.rahul.k@gmail.com>
2015-12-06 16:59:45 +0530

    Update config-prod.js

--- config/config-prod.js
+++ config/config-prod.js
@@ -5,14 +5,14 @@ config.dp_port = "";
 config.PORT = 80;
 
 config.AWS = {
-		accessKeyId : "AKIAIESZWA4O2NVRCHHQ",
-		secretAccessKey : "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",
-		region : "ap-northeast-1",
+		accessKeyId : "****",
+		secretAccessKey : "****",
+		region : "****",
 		bucket : "complaindia.com"
 };
 
 config.GMap = {
-		key : "AIzaSyCux-OQmC6cHC7dMRDEkZDNuKw7FAickhI"
+		key : "****"
 };
 
 module.exports = config;
